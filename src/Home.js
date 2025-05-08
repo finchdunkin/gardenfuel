9722716f7cc1fe6741ff1b7b58d1445227fce7af
finchdunkin <finch.dunkin@gmail.com>
2025-05-07 22:22:44 -0500

    d

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to GardenFuel 🌿</h1>
+      <p>Select an area above to begin your journey. Let’s make food fun, flavorful, and focused on your goals.</p>
+    </div>
+  );
+}
+
+export default Home;
